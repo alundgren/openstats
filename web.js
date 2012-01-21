3a73ad7812a20f8582c845f1b3b7ef244558a2ff
Andreas Lundgren <andreas.lundgren@gmail.com>
2012-01-21 19:13:22 +0100

    Simple test, top 10 total aid countries over the last years

--- web.js
+++ web.js
@@ -1,9 +1,40 @@
 var express = require('express');
+var http = require('http');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/country/:year', function (request, response) {
+    var options = {
+        host: 'api.openaid.se', //putting http:// here causes node to explode
+        port: 80,
+        path: '/api/v1/country?year=' + parseInt(request.params.year, 10),
+        method: 'GET'
+    };
+    var chunks = [];
+    http.request(options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            chunks.push(chunk);
+        });
+        res.on('end', function () {
+            var jsonData = JSON.parse(chunks.join(''));
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify(jsonData), 'utf-8');
+        });
+    }).end();
+});
+app.get('/', function (request, response) {
+    fs.readFile('./index.html', function (error, content) {
+        if (error) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        }
+    });
 });
 
 var port = process.env.PORT || 3000;
